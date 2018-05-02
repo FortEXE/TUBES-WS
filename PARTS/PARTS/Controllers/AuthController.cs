894d26bce47bbfb51ed745ffb1249e43a9a6b13c
shrkboy <ilhamn33@gmail.com>
2018-05-02 13:23:22 +0700

    Update AuthController

--- PARTS/PARTS/Controllers/AuthController.cs
+++ PARTS/PARTS/Controllers/AuthController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace PARTS.Controllers
 {
@@ -14,9 +18,26 @@ namespace PARTS.Controllers
         [HttpPost ("token")]
         public IActionResult Token()
         {
-            String tokenString = "test";
+            var headers = Request.Headers["Authorization"];
+            if (headers.ToString().StartsWith("Basic"))
+            {
+                var credValue = headers.ToString().Substring("Basic".Length).Trim();
 
-            return Ok(tokenString);
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SecretKey!@#12364488993_====++++"));
+                var signInCred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+                var ClaimsData = new[] { new Claim(ClaimTypes.Name, "username") };
+                var token = new JwtSecurityToken(
+                    issuer: "computercorner.com",
+                    audience: "computercorner.com",
+                    expires: DateTime.Now.AddMinutes(1),
+                    claims: ClaimsData,
+                    signingCredentials: signInCred
+                    );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(tokenString);
+            }
+            return BadRequest("Wrong Token");
         }
     }
 }
